898bec91b61243e0668105d008c6e39808a0c9e2
Rafijoee <rafijoe44@gmail.com>
2024-06-14 10:08:19 +0700

    LAST PUSHHHHHHH

--- resources/views/blog.php
+++ resources/views/blog.php
@@ -69,6 +69,8 @@
                     <button type="submit" class="text-white bg-muda bulet hover-button focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5">Kirim</button>
                 </form>
 
+                
+                
                 <?php
                 $commentPost = [];
 
