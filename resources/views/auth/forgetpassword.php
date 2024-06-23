28f653c8a10965814dbee52728c0fa393c0efc4e
Rafijoee <rafijoe44@gmail.com>
2024-06-23 21:17:20 +0700

    feat forget password

---
+++ resources/views/auth/forgetpassword.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. At eius vel possimus placeat aut. Sapiente, aut! Enim error ipsa animi sit a earum modi quo, quas possimus. Ratione rem error atque, maxime quos omnis consequatur quaerat itaque dolorum veritatis temporibus voluptates ullam, numquam dolorem! Dolorem voluptate eos sint impedit aperiam perferendis et, facere esse nulla. Velit praesentium magnam sit mollitia, eius maiores adipisci assumenda ab dolor consectetur quisquam obcaecati ipsum et at animi error voluptate dolorum quasi debitis fugit modi cumque illum fugiat! Impedit necessitatibus illo, rerum dolore eaque et deleniti quod consectetur amet error magnam, enim ipsa. Exercitationem autem minima, quod error tempora enim totam soluta eveniet vero doloribus voluptatibus qui commodi incidunt quos quo nesciunt, laudantium sunt. Consequatur, cumque praesentium aliquid porro obcaecati dicta perspiciatis molestiae esse quae facilis doloremque error aliquam, expedita modi fuga nesciunt aperiam sunt, corporis repellendus? Soluta doloremque eius expedita natus. Veniam similique, fugiat sequi alias iusto facilis in aliquam, accusantium fuga animi ipsum nihil error! Quod repudiandae similique aperiam delectus recusandae accusantium magni quisquam consequuntur nihil. Natus unde aut perferendis ab culpa voluptatibus quod aspernatur perspiciatis inventore! Tempora dolorum vel voluptatem quas eos molestias quae, perspiciatis et vero, tenetur dolore unde natus ut.
+</body>
+</html>
